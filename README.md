ae9f9e47d42816d9952e8d4c459513529ac3aca3
Smart-Space <3180471716@qq.com>
2024-07-12 10:28:47 +0800

    更新说明文档

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Pip Integration Platform
+
+使用python和TinUI（tkinter）编写的pip图形化管理器。
---
+++ compile.cmd
@@ -0,0 +1,1 @@
+nuitka --mingw64 --standalone --output-dir="E:\PyApp\Pip-Integration-Platform" --include-data-file=pages/*.xml=pages/ --windows-console-mode=disable --show-progress --plugin-enable=tk-inter .\main.py
